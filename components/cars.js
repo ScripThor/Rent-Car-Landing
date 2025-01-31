84430549784aeaba1b07b385b3c99026369a1237
Skjeld <a.dreake@yandex.ru>
2025-01-31 22:08:54 +0300

    Вынес модалку в отдельный компонент

--- components/cars.js
+++ components/cars.js
@@ -60,22 +60,6 @@ class CarsComponent extends HTMLElement {
           </div>
         </div>
         <div id="carList" class="car-list"></div>
-        <div class="modal">
-          <div class="modal-content">
-            <form id="feedbackForm">
-              <div class="feedbackForm-message" id="feedbackForm-message"></div>
-              <label class="modal-label" for="name">Имя:</label>
-              <input class="modal-input" type="text" id="name" name="name" required>
-              <label class="modal-label" for="tel">Телефон:</label>
-              <input class="modal-input" type="tel" id="tel" name="tel" required>
-              <label class="modal-label" for="message">Комментарий:</label>
-              <textarea id="message" name="message" required></textarea>
-              <button type="submit">Отправить</button>
-            </form>
-            <span onclick="closeModal();" class="modal-close">&times;</span>
-          </div>
-        </div>
-        <div id="feedbackInfo" class="feedback-info hidden"></div>
       </div>
     </section>
     `;
